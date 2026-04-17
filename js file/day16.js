31c3dabb87089200a1f3c263035345bc075ffb75
Mohsin Chohan <mohsinchohan7778@gmail.com>
2026-04-18 04:49:33 +0500

    add some new features

---
+++ js file/day16.js
@@ -0,0 +1,8 @@
+// This file contain the concept of async and await in JavaScript
+
+async function greet(){
+    return "Mohsin";
+}
+ const result = greet();
+ result.then((value) => {console.log(value);})
+ 
